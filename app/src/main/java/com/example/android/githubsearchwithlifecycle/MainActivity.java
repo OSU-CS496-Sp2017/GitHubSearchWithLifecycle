0b0ac388e37fa704e4b398771ca96f7d873d7cdc
Rob Hess <robwhess@gmail.com>
2017-05-16 17:49:45 -0700

    Add scratch code from 5/16 lecture.

--- app/src/main/java/com/example/android/githubsearchwithlifecycle/MainActivity.java
+++ app/src/main/java/com/example/android/githubsearchwithlifecycle/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.githubsearchwithlifecycle;
 
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -21,21 +24,26 @@ import com.example.android.githubsearchwithlifecycle.utils.NetworkUtils;
 import java.io.IOException;
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity implements GitHubSearchAdapter.OnSearchResultClickListener {
+public class MainActivity extends AppCompatActivity
+        implements GitHubSearchAdapter.OnSearchResultClickListener, LoaderManager.LoaderCallbacks<String> {
+
+    private static final String SEARCH_RESULTS_LIST_KEY = "searchResultsList";
+    private static final String SEARCH_URL_KEY = "githubSearchURL";
 
     private RecyclerView mSearchResultsRV;
     private EditText mSearchBoxET;
     private ProgressBar mLoadingIndicatorPB;
     private TextView mLoadingErrorMessageTV;
-    private Toast mSearchResultToast;
     private GitHubSearchAdapter mGitHubSearchAdapter;
+
+    private ArrayList<GitHubUtils.SearchResult> mSearchResultsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSearchResultToast = null;
+        mSearchResultsList = null;
 
         mSearchBoxET = (EditText)findViewById(R.id.et_search_box);
         mLoadingIndicatorPB = (ProgressBar)findViewById(R.id.pb_loading_indicator);
@@ -47,6 +55,11 @@ public class MainActivity extends AppCompatActivity implements GitHubSearchAdapt
 
         mGitHubSearchAdapter = new GitHubSearchAdapter(this);
         mSearchResultsRV.setAdapter(mGitHubSearchAdapter);
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(SEARCH_RESULTS_LIST_KEY)) {
+            mSearchResultsList = (ArrayList)savedInstanceState.getSerializable(SEARCH_RESULTS_LIST_KEY);
+            mGitHubSearchAdapter.updateSearchResults(mSearchResultsList);
+        }
 
         Button searchButton = (Button)findViewById(R.id.btn_search);
         searchButton.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +73,14 @@ public class MainActivity extends AppCompatActivity implements GitHubSearchAdapt
         });
     }
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mSearchResultsList != null) {
+            outState.putSerializable(SEARCH_RESULTS_LIST_KEY, mSearchResultsList);
+        }
+    }
+
     private void doGitHubSearch(String searchQuery) {
         String githubSearchUrl = GitHubUtils.buildGitHubSearchURL(searchQuery);
         Log.d("MainActivity", "got search url: " + githubSearchUrl);
@@ -71,6 +92,54 @@ public class MainActivity extends AppCompatActivity implements GitHubSearchAdapt
         Intent intent = new Intent(this, SearchResultDetailActivity.class);
         intent.putExtra(GitHubUtils.SearchResult.EXTRA_SEARCH_RESULT, searchResult);
         startActivity(intent);
+    }
+
+    @Override
+    public Loader<String> onCreateLoader(int id, final Bundle args) {
+        return new AsyncTaskLoader<String> (this) {
+
+            @Override
+            protected void onStartLoading() {
+                if (args != null) {
+                    mLoadingIndicatorPB.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public String loadInBackground() {
+                if (args != null) {
+                    String githubSearchUrl = args.getString(SEARCH_URL_KEY);
+                    String searchResults = null;
+                    try {
+                        searchResults = NetworkUtils.doHTTPGet(githubSearchUrl);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return searchResults;
+                } else {
+                    return null;
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onLoadFinished(Loader<String> loader, String data) {
+        mLoadingIndicatorPB.setVisibility(View.INVISIBLE);
+        if (data != null) {
+            mLoadingErrorMessageTV.setVisibility(View.INVISIBLE);
+            mSearchResultsRV.setVisibility(View.VISIBLE);
+            mSearchResultsList = GitHubUtils.parseGitHubSearchResultsJSON(data);
+            mGitHubSearchAdapter.updateSearchResults(mSearchResultsList);
+        } else {
+            mSearchResultsRV.setVisibility(View.INVISIBLE);
+            mLoadingErrorMessageTV.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<String> loader) {
+        // Nothing to do...
     }
 
     public class GitHubSearchTask extends AsyncTask<String, Void, String> {
@@ -98,8 +167,8 @@ public class MainActivity extends AppCompatActivity implements GitHubSearchAdapt
             if (s != null) {
                 mLoadingErrorMessageTV.setVisibility(View.INVISIBLE);
                 mSearchResultsRV.setVisibility(View.VISIBLE);
-                ArrayList<GitHubUtils.SearchResult> searchResultsList = GitHubUtils.parseGitHubSearchResultsJSON(s);
-                mGitHubSearchAdapter.updateSearchResults(searchResultsList);
+                mSearchResultsList = GitHubUtils.parseGitHubSearchResultsJSON(s);
+                mGitHubSearchAdapter.updateSearchResults(mSearchResultsList);
             } else {
                 mSearchResultsRV.setVisibility(View.INVISIBLE);
                 mLoadingErrorMessageTV.setVisibility(View.VISIBLE);
